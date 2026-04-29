70d82bcbf147edebcefffc43a2dc731c852b8134
eccia85-ops <eccia85@gmail.com>
2026-04-29 18:33:33 +0900

    Update sources.py

--- config/sources.py
+++ config/sources.py
@@ -1,10 +1,18 @@
 RSS_SOURCES = [
     {
-        "site": "DailyPharm",
+        "site": "데일리팜",
         "url": "https://www.dailypharm.com/rss/allArticle.xml"
     },
     {
-        "site": "HitNews",
-        "url": "https://www.hitnews.co.kr/rss/allArticle.xml"
+        "site": "약사공론",
+        "url": "https://www.kpanews.co.kr/rss/allArticle.xml"
+    },
+    {
+        "site": "의학신문",
+        "url": "http://www.bosa.co.kr/rss/allArticle.xml"
+    },
+    {
+        "site": "메디칼타임즈",
+        "url": "https://www.medicaltimes.com/rss/allArticle.xml"
     }
 ]
