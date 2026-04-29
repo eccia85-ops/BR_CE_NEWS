84c2b0dc6c1ae11b5ef53a05f65285cd59f0f9f9
eccia85-ops <eccia85@gmail.com>
2026-04-29 18:27:40 +0900

    Create sources.py

---
+++ config/sources.py
@@ -0,0 +1,10 @@
+RSS_SOURCES = [
+    {
+        "site": "DailyPharm",
+        "url": "https://www.dailypharm.com/rss/allArticle.xml"
+    },
+    {
+        "site": "HitNews",
+        "url": "https://www.hitnews.co.kr/rss/allArticle.xml"
+    }
+]
