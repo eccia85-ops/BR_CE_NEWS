0d1672d9041f80d645caf2563e6235f0c0d461b1
eccia85-ops <eccia85@gmail.com>
2026-04-29 18:16:14 +0900

    Update test.py

--- api/test.py
+++ api/test.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
-@app.get("/")
-def test():
-    return {"status": "OK"}
+@app.api_route("/{path:path}", methods=["GET", "POST"])
+async def catch_all(request: Request, path: str):
+    return {
+        "received_path": request.url.path,
+        "matched_path_param": path
+    }
