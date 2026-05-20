e1f842e8c8713c6a32cb4bae758fbba8ced087b7
eccia85-ops <eccia85@gmail.com>
2026-05-20 16:51:47 +0900

    Update manage.py

--- api/manage.py
+++ api/manage.py
@@ -9,6 +9,7 @@ HTML = """<!DOCTYPE html>
   <meta charset="utf-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
   <title>CE 뉴스 관리</title>
+  <script defer src="/_vercel/insights/script.js"></script>
   <style>
     :root {
       --primary:       #1a56db;
